4b2c9e2d54d311318352a8c34534cbeb11f86528
rupert van niekerk <127295503+rupert08@users.noreply.github.com>
2024-08-29 21:00:10 +0200

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 5116,  //run cmd command propt for open ports {netstat -aon}
+    port: 5119,  //run cmd command propt for open ports {netstat -aon}
     proxy: {
       '/ecommerce' : {
         target : 'http://localhost:8082',
